4eb9d4403c64b43784e6457b7703f9985eadb084
TommyWood333 <thomas.wood333@gmail.com>
2026-05-18 14:40:39 -0400

    Update strava-callback.js

--- netlify/functions/strava-callback.js
+++ netlify/functions/strava-callback.js
@@ -1,3 +1,43 @@
+const https = require('https');
+
+exports.handler = async (event) => {
+  const code = event.queryStringParameters?.code;
+  const error = event.queryStringParameters?.error;
+
+  if (error) {
+    return {
+      statusCode: 302,
+      headers: { Location: '/?error=' + error },
+      body: ''
+    };
+  }
+
+  if (!code) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error: 'No authorization code'})
+    };
+  }
+
+  try {
+    const tokenResponse = await exchangeCode(code);
+    const athlete = tokenResponse.athlete;
+    
+    const redirectUrl = `/?strava_token=${tokenResponse.access_token}&athlete_name=${encodeURIComponent(athlete.firstname + ' ' + athlete.lastname)}&athlete_id=${athlete.id}`;
+    
+    return {
+      statusCode: 302,
+      headers: { Location: redirectUrl },
+      body: ''
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({error: err.message})
+    };
+  }
+};
+
 function exchangeCode(code) {
   return new Promise((resolve, reject) => {
     const postData = JSON.stringify({
@@ -7,6 +47,7 @@ function exchangeCode(code) {
       grant_type: 'authorization_code',
       redirect_uri: 'https://testversionrunningplan.netlify.app/.netlify/functions/strava-callback'
     });
+
     const options = {
       hostname: 'www.strava.com',
       port: 443,
@@ -17,6 +58,7 @@ function exchangeCode(code) {
         'Content-Length': postData.length
       }
     };
+
     const req = https.request(options, (res) => {
       let data = '';
       res.on('data', chunk => data += chunk);
@@ -28,6 +70,7 @@ function exchangeCode(code) {
         }
       });
     });
+
     req.on('error', reject);
     req.write(postData);
     req.end();
